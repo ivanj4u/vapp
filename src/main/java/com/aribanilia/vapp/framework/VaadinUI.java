46ca858ad603426becdca57945d12e69a16ce6c7
Ivan Aribanilia <angko.j4u@gmail.com>
2017-10-31 22:46:23 +0700

    add logo

--- src/main/java/com/aribanilia/vapp/framework/VaadinUI.java
+++ src/main/java/com/aribanilia/vapp/framework/VaadinUI.java
@@ -10,22 +10,37 @@ import com.aribanilia.vapp.model.LoginPage;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.navigator.Navigator;
+import com.vaadin.server.DefaultErrorHandler;
 import com.vaadin.server.Responsive;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.spring.navigator.SpringViewProvider;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.themes.ValoTheme;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
 @Theme(ValoTheme.THEME_NAME)
 @Title("Vaadin Spring")
 public class VaadinUI extends UI {
-    @Autowired private SpringViewProvider viewProvider;
+    private final SpringViewProvider viewProvider;
+
+    private static final Logger logger = LoggerFactory.getLogger(VaadinUI.class);
+
+    @Autowired
+    public VaadinUI(SpringViewProvider viewProvider) {
+        this.viewProvider = viewProvider;
+    }
 
     @Override
     protected void init(VaadinRequest request) {
+        setErrorHandler(event -> {
+            Throwable t = DefaultErrorHandler.findRelevantThrowable(event.getThrowable());
+            logger.error("Error during request", t);
+        });
+
         Responsive.makeResponsive(this);
         addStyleName(ValoTheme.UI_WITH_MENU);
 
